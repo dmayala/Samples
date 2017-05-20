dfb0472343bd5a96d058e9699b50fd6b90c49987
Daniel Ayala <dmayala001@gmail.com>
2017-05-19 20:24:55 -0500

    add MVC routes and static files

--- Samples/Startup.cs
+++ Samples/Startup.cs
@@ -37,7 +37,14 @@ namespace Samples
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
